acf7bf2e93cbd323b1781aeeded7903ec23e3ff2
Vasileva Daria <Dasha0105200738@yandex.ru>
2025-10-05 12:00:57 +0300

    Добавил вывод суммы введенных чисел

--- program.py
+++ program.py
@@ -1,3 +1,5 @@
 print (Hello, World!)
  num1 = int(input("Введите первое число: "))
-    num2 = int(input("Введите второе число: "))   
+    num2 = int(input("Введите второе число: "))
+     result = num1 + num2
+       print(f"Сумма чисел: {result}")
